ddceaa27a79e96aee56d16aa2453acd759f98727
aggicreative555 <aggi.creative@gmail.com>
2024-05-01 21:49:23 +0200

    feat(login): async function to store password token

--- js/login.js
+++ js/login.js
@@ -26,3 +26,30 @@ const userLogin = {
 };
 
 const loginUrl = `${API_BASE_URL}/auth/login`;
+
+loginUser(loginUrl, userLogin);
+
+async function getWithToken(url) {
+    try {
+        const token = localStorage.getItem('accessToken');
+        console.log(token);
+        const fetchOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`, 
+            },
+        }
+
+        const response = await fetch(url, fetchOptions);
+        const json = await response.json();
+        console.log(response);
+        console.log(json);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+const postsUrl = `${API_BASE_URL}/social/posts`;
+
+getWithToken(postsUrl);
