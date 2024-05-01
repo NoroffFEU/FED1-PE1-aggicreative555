ad874b923c0bb86c7068f9d276114f1693b7f806
aggicreative555 <aggi.creative@gmail.com>
2024-05-01 21:40:57 +0200

    feat(register): register user function using api fetch

--- js/register.js
+++ js/register.js
@@ -112,3 +112,24 @@ function hashPassword(password) {
 //     return true;
 // }
 
+const API_BASE_URL = 'https://v2.api.noroff.dev';
+async function registerUser(url, userData) {
+    try {
+        const postData = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        };
+        
+        const response = await fetch(url, postData);
+        const json = await response.json();
+        console.log(json);
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const registerUrl = `${API_BASE_URL}/auth/register`;
