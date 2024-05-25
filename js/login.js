bf9181bab42763e88e4e439599f984417fd69880
aggicreative555 <aggi.creative@gmail.com>
2024-05-25 18:29:42 +0200

    feat(login): Add login functionality #3
    Take user input from fields instead of hardcoded values

--- js/login.js
+++ js/login.js
@@ -18,44 +18,69 @@ async function loginUser(url, userData) {
         const json = await response.json();
         console.log(response);
         console.log(json);
-        const accessToken = json.data.accessToken;
-        localStorage.setItem('accessToken', accessToken);
 
-    } catch(error) {
+        if (json.success) {
+            const accessToken = json.data.accessToken;
+            localStorage.setItem('accessToken', accessToken);
+        }
+
+        window.location.href = '../post/edit.html';
+
+    } else {
+        alert("Login failed. User doesn't exist");
+        } 
+        
+    catch(error) {
         console.error(error);
     }
 }
 
-const userLogin = {
-    email: "aggi.123@stud.noroff.no", // Required
-    password: "aggi12345678" // Required
-};
+// async function getWithToken(url) {
+//     try {
+//         const token = localStorage.getItem('accessToken');
+//         console.log(token);
+//         const fetchOptions = {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': `Bearer ${token}`, 
+//             },
+//         }
 
-const loginUrl = `${API_BASE_URL}/auth/login`;
+//         const response = await fetch(url, fetchOptions);
+//         const json = await response.json();
+//         console.log(response);
+//         console.log(json);
 
-loginUser(loginUrl, userLogin);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-async function getWithToken(url) {
-    try {
-        const token = localStorage.getItem('accessToken');
-        console.log(token);
-        const fetchOptions = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`, 
-            },
-        }
+function handleLogin() {
+    
+    const loginEmail = document.getElementById('loginEmail').value;
+    const loginPassword = document.getElementById('loginPassword').value;
 
-        const response = await fetch(url, fetchOptions);
-        const json = await response.json();
-        console.log(response);
-        console.log(json);
+    
+    const userLogin = {
+        email: loginEmail, 
+        password: loginPassword 
+    };
 
-    } catch (error) {
-        console.log(error);
-    }
+   
+    const loginUrl = `${API_BASE_URL}/auth/login`;
+
+    loginUser(loginUrl, userLogin);
 }
-const postsUrl = `${API_BASE_URL}/social/posts`;
 
-getWithToken(postsUrl);
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+    
+    handleLogin();
+});
+
+// const postsUrl = `${API_BASE_URL}/auth/login`;
+
+// getWithToken(postsUrl);
