2a3bb6f58d15c3da3aaee73fb4be9c5b8a096659
aggicreative555 <aggi.creative@gmail.com>
2024-05-27 15:53:53 +0200

    feat(create post): Add update post function

--- js/posts/create.mjs
+++ js/posts/create.mjs
@@ -7,17 +7,22 @@ const API_BASE_URL = 'https://v2.api.noroff.dev';
 
 const action = "/social/posts";
 
-export async function createPost(postData) {
-    const createPostUrl = API_BASE_URL + action;
+export async function updatePost(postData) {
+    const updatePostUrl = `${API_BASE_URL}${action}/${postData.id}`;
+    console.log("updatePostUrl:", updatePostUrl);
     
 
-    const response = await authFetch(createPostUrl, {
+    const response = await authFetch(updatePostUrl, {
         method: "post",
         body: JSON.stringify(postData)
     })
+
+    if (!postData.id) {
+        console.error("postData does not contain an id:", postData);
+        return;
+    }
 
     const post = await response.json();
 
     console.log(post);
 }
-
