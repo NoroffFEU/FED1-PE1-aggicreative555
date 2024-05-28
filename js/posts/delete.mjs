c8afb873ba11c63b82c28321b8a692f2dfee4595
aggicreative555 <aggi.creative@gmail.com>
2024-05-28 23:09:54 +0200

    refractor(delete): Refractor delete function

--- js/posts/delete.mjs
+++ js/posts/delete.mjs
@@ -2,22 +2,35 @@ import { authFetch } from "../authFetch.mjs";
 
 const action = "/blog/posts";
 
-const API_BASE_URL = 'https://v2.api.noroff.dev';
+const API_BASE_URL = "https://v2.api.noroff.dev";
 
+function parseSearchParams(searchParamsString) {
+	const searchParamsArray = searchParamsString.split("&");
+
+	if (searchParamsArray.length === 0) {
+		return {};
+	}
+
+	const category = searchParamsArray[0];
+	const id = searchParamsArray[1];
+
+	const result = {
+		category: category.slice(1),
+		id: id,
+	};
+
+	return result;
+}
+
+const searchParamsString = window.location.search;
+const parsedSearchParams = parseSearchParams(searchParamsString);
 export async function removePost() {
+	const updatePostUrl = `${API_BASE_URL}${action}/${parsedSearchParams.category}/${parsedSearchParams.id}`;
+	console.log("updatePostUrl:", updatePostUrl);
 
-    const updatePostUrl = `${API_BASE_URL}${action}/${id}`;
-    console.log("updatePostUrl:", updatePostUrl);
-    
-
-    const response = await authFetch(updatePostUrl, {
-        method: "delete"
-    })
-
-    if (!postData.id) {
-        console.error("postData does not contain an id:", postData);
-        return;
-    }
-
-   return await response.json();
+	const response = await authFetch(updatePostUrl, {
+		method: "delete",
+	});
+	window.location.href = "/";
+	return await response.json();
 }
