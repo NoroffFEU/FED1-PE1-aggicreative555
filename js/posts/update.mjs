62d7a9b77f634ee93737c3a2bd86249ac9c8a5e5
aggicreative555 <aggi.creative@gmail.com>
2024-05-28 23:09:08 +0200

    refractor(update): Refractor update post function

--- js/posts/update.mjs
+++ js/posts/update.mjs
@@ -1,23 +1,36 @@
 import { authFetch } from "../authFetch.mjs";
-
-const API_BASE_URL = 'https://v2.api.noroff.dev';
+const API_BASE_URL = "https://v2.api.noroff.dev";
 
 const action = "/blog/posts";
+function parseSearchParams(searchParamsString) {
+	const searchParamsArray = searchParamsString.split("&");
+
+	if (searchParamsArray.length === 0) {
+		return {};
+	}
+
+	const category = searchParamsArray[0];
+	const id = searchParamsArray[1];
+
+	const result = {
+		category: category.slice(1),
+		id: id,
+	};
+
+	return result;
+}
+
+const searchParamsString = window.location.search;
+const parsedSearchParams = parseSearchParams(searchParamsString);
 
 export async function updatePost(postData) {
+	const updatePostUrl = `${API_BASE_URL}${action}/${parsedSearchParams.category}/${parsedSearchParams.id}`;
+	console.log("updatePostUrl:", updatePostUrl);
 
-    if (!postData.id) {
-        console.error("postData does not contain an id:", postData);
-        return;
-    }
-    const updatePostUrl = `${API_BASE_URL}${action}/${postData.id}`;
-    console.log("updatePostUrl:", updatePostUrl);
-    
+	const response = await authFetch(updatePostUrl, {
+		method: "put",
+		body: JSON.stringify(postData),
+	});
 
-    const response = await authFetch(updatePostUrl, {
-        method: "put",
-        body: JSON.stringify(postData)
-    })
-
-   return await response.json();
+	return await response.json();
 }
