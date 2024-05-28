bba95c4aa6f5f7e701e4cdfcecb5724f4ed7659f
aggicreative555 <aggi.creative@gmail.com>
2024-05-28 23:10:25 +0200

    refractor(create): Refractor create post function

--- js/posts/create.mjs
+++ js/posts/create.mjs
@@ -1,43 +1,50 @@
 
 import { authFetch } from "../authFetch.mjs";
-
-const API_BASE_URL = 'https://v2.api.noroff.dev';
+import { load } from "../storage.mjs";
+const API_BASE_URL = "https://v2.api.noroff.dev";
 
 const action = "/blog/posts";
-
+const profile = JSON.parse(load("profile"));
+console.log(profile);
 export async function createPost(postData) {
+	if (!postData.name) {
+		console.error("postData must contain name", postData);
+		return;
+	}
 
-    if (!postData.name) {
-        console.error("postData must contain name", postData);
-        return;
-    }
+	const createPost = `${API_BASE_URL}${action}/${postData.name}`;
 
-    const createPost = `${API_BASE_URL}${action}/${postData.name}`;
+	console.log("createPost:", createPost);
 
-    console.log("createPost:", createPost);
-    
+	const response = await authFetch(createPost, {
+		method: "post",
+		body: JSON.stringify(postData),
+	});
 
-    const response = await authFetch(createPost, {
-        method: "post",
-        body: JSON.stringify(postData)
-    })
-
-        const post = await response.json();
-        return post;
+	const post = await response.json();
+	return post;
 }
 
+document
+	.getElementById("createPostForm")
+	.addEventListener("submit", async (e) => {
+		e.preventDefault();
 
-const postData = {
-    name: "example-post-name",
-    title: "Example Title",
-    body: "Example body content",
-    tags: ["example", "post"],
-    media: {
-        url: "https://url.com/image.jpg",
-        alt: "Example image description"
-    }
-};
+		const postTitle = document.getElementById("postTitle").value;
+		const postBody = document.getElementById("postBody").value;
+		const postImageUrl = document.getElementById("postImageUrl").value;
+		const postImageAlt = document.getElementById("postImageAlt").value;
 
-createPost(postData).then(post => {
-    console.log("Created Post:", post);
-});
+		const postData = {
+			name: profile.name,
+			title: postTitle,
+			body: postBody,
+			tags: ["example", "post"],
+			media: {
+				url: postImageUrl,
+				alt: postImageAlt,
+			},
+		};
+
+		createPost(postData);
+	});
